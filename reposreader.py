5275c4d16293d66f55b62bcbacef92c21984b03d
Tyler Band <tylerm007@gmail.com>
2023-05-16 20:43:36 -0400

    fix main

--- reposreader.py
+++ reposreader.py
@@ -620,7 +620,7 @@ command = "not set"
 sections = "all" # all is default
 
 if __name__ == "__main__":
-#   main()
+    main()
 #else:  
 #    local testing and debugging
-    listDirs(basepath, sections)
+#    listDirs(basepath, sections)
