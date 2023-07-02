1a54ca628e2abf203842811a54691bf4ae2a7570
Tyler Band <tylerm007@gmail.com>
2023-07-02 10:10:30 -0400

    add sample run.sh command

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
-python3 reposreader.py --project b2bderbynw --repos /Users/tylerband/CALiveAPICreator.repository --section all
+#bin/bash 
+# This is a sample on how to create conversion files for CA Live API Creator to API Logic Server
+
+rm *.txt
+export project=b2bderbynw
+
+python3 reposreader.py --project $project --repos /Users/tylerband/CALiveAPICreator.repository --section security > declare_security.txt
+
+python3 reposreader.py --project $project --repos /Users/tylerband/CALiveAPICreator.repository --section rules > declare_logic.txt
+
+python3 reposreader.py --project $project --repos /Users/tylerband/CALiveAPICreator.repository --section resources > customize_api.txt
+
+python3 reposreader.py --project $project --repos /Users/tylerband/CALiveAPICreator.repository --section data_sources > DataSources.txt
+
+python3 reposreader.py --project $project --repos /Users/tylerband/CALiveAPICreator.repository --section all > allReport.txt
