85295d4dddd91e97979ef6ca14a001364ee2a5f6
Tyler Band <tylerm007@gmail.com>
2023-08-01 11:22:40 -0400

    add windows bat file

--- run.bat
+++ run.bat
@@ -2,8 +2,8 @@
 # This is a sample on how to create conversion files for CA Live API Creator to API Logic Server
 
 rm *.txt
-export project=b2bderbynw
-export repos=/Users/tylerband/CALiveAPICreator.repository
+project=b2bderbynw
+repos=C:\CALiveAPICreator.repository
 
 python3 reposreader.py --project $project --repos $repos --section security > declare_security.txt
 
